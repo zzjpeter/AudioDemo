aec8ebbe7924503c96d0e3fe73e9304bb61a594e
zhuzhijia <zhuzhijia@lespark.us>
2019-05-05 10:13:38 +0800

    Initial Commit

---
+++ AudioDemo/AudioTool.h
@@ -0,0 +1,17 @@
+//
+//  AudioTool.h
+//  AudioDemo
+//
+//  Created by 朱志佳 on 2019/5/5.
+//  Copyright © 2019 朱志佳. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AudioTool : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AudioDemo/AudioTool.m
@@ -0,0 +1,13 @@
+//
+//  AudioTool.m
+//  AudioDemo
+//
+//  Created by 朱志佳 on 2019/5/5.
+//  Copyright © 2019 朱志佳. All rights reserved.
+//
+
+#import "AudioTool.h"
+
+@implementation AudioTool
+
+@end
