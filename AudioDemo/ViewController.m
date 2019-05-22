86cb50db3e4f3319bc4c0e622fcb6b33fc280579
zhuzhijia <zhuzhijia@lespark.us>
2019-05-22 17:30:39 +0800

    AudioEncodeH264Manager test ok

--- AudioDemo/ViewController.m
+++ AudioDemo/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 #import "AudioManager.h"
+#import "AudioEncodeH264Manager.h"
 
 @interface ViewController ()
+
+@property (nonatomic,strong) UIView *playView;
 
 @end
 
@@ -25,6 +28,24 @@
 }
 - (IBAction)stopAction:(id)sender {
     [[AudioManager sharedAudioManager] stop];
+}
+- (IBAction)encodeH264Start:(id)sender {
+    [self.view addSubview:self.playView];
+    [AudioEncodeH264Manager sharemanager].playView = self.playView;
+    [[AudioEncodeH264Manager sharemanager] play];
+}
+- (IBAction)encodeH264Stop:(id)sender {
+    [[AudioEncodeH264Manager sharemanager] stop];
+}
+
+- (UIView *)playView
+{
+    if (!_playView) {
+        UIView *playView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 300, 300)];
+        playView.bottom = self.view.bottom;
+        _playView = playView;
+    }
+    return _playView;
 }
 
 @end
