6d0f09a1836279b6cfbcdbf580cde15511e0667a
zhuzhijia <zhuzhijia@lespark.us>
2019-08-21 11:15:22 +0800

    Pcm:音频原始数据 AAC:音频aac编码数据 H264:视频h.264编码数据

---
+++ AudioDemo/Audio/Stream/Client/StreamClientManager.h
@@ -0,0 +1,19 @@
+//
+//  StreamClientManager.h
+//  AudioDemo
+//
+//  Created by 朱志佳 on 2019/6/5.
+//  Copyright © 2019 朱志佳. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AVHeader.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StreamClientManager : NSObject
+
+SingleInterface(manager)
+
+@end
+
+NS_ASSUME_NONNULL_END
